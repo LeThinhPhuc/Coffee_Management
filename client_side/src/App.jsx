abf128475c94e97520c000ad4852df0fca591315
Tăng Khải Minh <tangkhaiminh2002@gmail.com>
2024-03-28 23:58:52 +0700

    npm install: tailwind, lodash, axios

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './components/Home/Home'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Home from "./components/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [count, setCount] = useState(0);
 
-  return (
-    <>
-      <div className="card">
-        <Home/>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="card">
+                <Home />
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
