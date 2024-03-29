e724ed639632e192056bc5323536b5f77876f8fa
Tăng Khải Minh <tangkhaiminh2002@gmail.com>
2024-03-29 12:35:34 +0700

    first commit

--- .history/client_side/src/App_20240328235803.jsx
+++ .history/client_side/src/App_20240328235803.jsx
@@ -8,15 +8,14 @@ function App() {
     const [count, setCount] = useState(0);
 
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <>
             <div className="card">
                 <Home />
                 <button onClick={() => setCount((count) => count + 1)}>
                     count is {count}
                 </button>
             </div>
-        </div>
+        </>
     );
 }
 
