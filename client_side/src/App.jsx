3daf30eaebb54100c784e4259f36b0c890abdf04
Tăng Khải Minh <tangkhaiminh2002@gmail.com>
2024-03-29 18:48:12 +0700

    add MenuItem, MenuModel, TemplateData, LeftMenu

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -3,21 +3,10 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Home from "./components/Home/Home";
+import LeftMenu from "./components/Menu/LeftMenu/LeftMenu";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div>
-            <h1 className="text-3xl font-bold underline">Hello world!</h1>
-            <div className="card">
-                <Home />
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-            </div>
-        </div>
-    );
+    return <LeftMenu></LeftMenu>;
 }
 
 export default App;
---
+++ client_side/src/components/Menu/LeftMenu/TemplateData.jsx
@@ -0,0 +1,46 @@
+const TemplateData = [
+    {
+        image: "https://thuytinhluminarc.com/wp-content/uploads/2022/08/hinh-tach-ca-phe-2.jpg",
+        name: "Cafe đen",
+        price: "17.000VNĐ",
+        desc: "2 cafe, 1 đường",
+    },
+    {
+        image: "https://thuytinhluminarc.com/wp-content/uploads/2022/08/hinh-tach-ca-phe-nong-va-banh.jpg",
+        name: "Cafe sữa",
+        price: "22.000VNĐ",
+        desc: "2 cafe, 1 đường, 1 sữa",
+    },
+    {
+        image: "https://thuytinhluminarc.com/wp-content/uploads/2022/08/hinh-ly-ca-phe-ve-trai-tim.jpg",
+        name: "Capuchino",
+        price: "35.000VNĐ",
+        desc: "2 kem, 1 cafe, 2 đường",
+    },
+    {
+        image: "https://thuytinhluminarc.com/wp-content/uploads/2022/08/hin-ly-ca-phe-sua-da-dep.jpg",
+        name: "Bạc xỉu nóng",
+        price: "22.000VNĐ",
+        desc: "1 cafe, 2 sữa, 1 đường",
+    },
+    {
+        image: "https://thuytinhluminarc.com/wp-content/uploads/2022/08/anh-ly-ca-phe-sua-pha-phin-1.jpg",
+        name: "Bạc xỉu đá",
+        price: "22.000VNĐ",
+        desc: "2 cafe, 1 đá, 1 đường",
+    },
+    {
+        image: "https://cdnmedia.baotintuc.vn/2017/08/29/16/00/giangw.jpg",
+        name: "Cacao nóng",
+        price: "25.000VNĐ",
+        desc: "1 cacao, 1 đường",
+    },
+    {
+        image: "https://toongcenter.vn/storage/photos/shares/meo%20pha%20che/tra%20dao%20cam%20s%E1%BA%A3/1.jpg",
+        name: "Trà đào",
+        price: "25.000VNĐ",
+        desc: "2 đào, 1 đường, 1 đá",
+    },
+];
+
+export default TemplateData;
