59d9dbe7ba9c58382d0d66f04c0d78646f280fbc
PhamTrieu0703 <thanhtrieupham10@gmail.com>
2024-04-06 08:31:39 +0700

    thêm thuộc tính

---
+++ client_side/src/components/Login_Signup/LoginSignup.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from "react" ;
+import './LoginSignup.css'
+
+import user_icon from '../Assets/person.png'
+import email_icon from '../Assets/email.png'
+import password_icon from '../Assets/password.png'
+import address_icon from '../Assets/address.png'
+import business_icon from '../Assets/business.png'
+
+const LoginSignup = () => {
+    const[action,setAction] = useState("Sign Up");
+    const[username,setUsername] = useState("");
+    const[password,setPassword] = useState("");
+    const account = {
+        'email' : "admin@gmail.com",
+        "password" : "123455"
+    }
+
+    const doLogin = () => {
+        if(username === account.email && password === account.password)
+        {
+            console.log("dang nhap thanh cong");
+        }
+        else console.log("that bai");
+
+    }
+     
+    return (
+      <div className='container'>
+        <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+        </div>
+        <div className="inputs">
+            {action==="Login"?<div></div>:
+                <div className="input">
+                <img src={user_icon} alt="" />
+                <input type="text" placeholder="Name" />
+            </div>
+               
+            }
+             {action==="Login"?<div></div>:
+             <div className="input">
+                <img src={business_icon} alt="" />
+                <input type="text" placeholder="Business" />
+            </div>
+            }   
+             {action==="Login"?<div></div>:
+             <div className="input">
+                <img src={address_icon} alt="" />
+                <input type="text" placeholder="Address" />
+            </div>
+            }       
+            <div className="input">
+                <img src={email_icon} alt="" />
+                <input type="email" placeholder="Email" value={username} onChange={(e) => setUsername(e.target.value)}/>
+            </div>
+            <div className="input">
+                <img src={password_icon} alt="" />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+            </div>
+        </div>
+        {action==="Sign Up"?<div></div>: <div className="forgot_password">Forgot Password? 
+        <span>Click here</span>  
+        <button className="btn_login" onClick={() => {
+            doLogin()
+        }}>Login</button> 
+        </div>}
+        {action==="Login"?<div></div>: <div className="change_signup">Register already? <span onClick={()=>{setAction("Login")}}>Login</span><button className="btn_signup">SignUp</button>
+</div>}
+
+        {action==="Sign Up"?<div></div>: <div className="text_dont_acccout">Dont have an accouut? <span  onClick={()=>{setAction("Sign Up")}}>SignUp</span></div>}
+        
+        </div>
+    );
+};
+export default LoginSignup
