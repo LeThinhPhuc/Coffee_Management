ecd7d4e8b18ed7cf5136d2f0ea94d58d2c77a4e1
thuhoa0810 <thuhoa08102003@gmail.com>
2024-04-08 22:43:49 +0700

    AS-39: Discount UI

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -1,21 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Home from "./components/Home/Home";
-import LeftMenu from "./components/Menu/LeftMenu/LeftMenu";
-import ManageMenu from "./components/manage/manageMenu/ManageMenu";
-import ManageIngredient from "./components/manage/manageIngredient/ManageIngredient";
-import Manage from "./components/manage/Manage";
-import Loading from "./components/manage/Loading";
 import { AppProvider } from "./context/MenuContext";
-import OrderPage from "./components/OrderPage/OrderPage";
+import Discount from "./components/Discount/Discount";
 function App() {
-    return (
-        <AppProvider>
-            <OrderPage></OrderPage>
-        </AppProvider>
-    );
+  return (
+    <AppProvider>
+      <Discount />
+    </AppProvider>
+  );
 }
 
 export default App;
--- client_side/src/components/Discount/Discount.jsx
+++ client_side/src/components/Discount/Discount.jsx
@@ -1,31 +1,86 @@
 import { useState } from "react";
-import Modal from "./Modal";
-import Table from "./Table";
-import Card from "./Card";
+import Modal from "./Modal/Modal";
+import Card from "./Card/Card";
 
 const Discount = () => {
   const [showModal, setShowModal] = useState(false);
+  const [lastId, setLastId] = useState(0);
 
   const handleClose = () => {
     setShowModal(false);
   };
 
+  const handleOpen = () => {
+    setShowModal(true);
+  };
+
+  const handleAdd = (newDiscount) => {
+    setLastId(discountArr.length + 1);
+    const discountWithId = { ...newDiscount, id: lastId };
+    setDiscountArr([...discountArr, discountWithId]);
+  };
+
+  const handleUpdate = (updatedDiscount) => {
+    //update the discountArr with the updatedDiscount
+    const updateDiscountArr = discountArr.map((discount) =>
+      discount.id === updatedDiscount.id ? updatedDiscount : discount
+    );
+    setDiscountArr(updateDiscountArr);
+    setShowModal(false);
+  };
+
+  const [discountArr, setDiscountArr] = useState([
+    {
+      id: 1,
+      code: "CHRISMAS20",
+      start: "10/04/2024",
+      end: "10/5/2024",
+      percent: 20,
+    },
+    {
+      id: 2,
+      code: "CHRISMAS20",
+      start: "10/04/2024",
+      end: "10/5/2024",
+      percent: 20,
+    },
+    {
+      id: 3,
+      code: "CHRISMAS20",
+      start: "10/04/2024",
+      end: "10/5/2024",
+      percent: 20,
+    },
+  ]);
+
   return (
-    <div className="flex-col justify-center">
+    <div className="flex-col justify-center py-16 px-10">
       <div className="flex justify-between mb-10 items-center ">
         <div className="text-4xl text-left ">Coupons & discounts </div>
         <button
           className="bg-gradient-to-r from-nude to-dark-nude hover:from-dark-nude hover:to-nude to- px-3 py-2 mr-2 rounded-md text-white "
-          onClick={() => {
-            setShowModal(true);
-          }}
+          onClick={handleOpen}
         >
           Add Code
         </button>
       </div>
-      <Card />
-
-      <Modal visible={showModal} onClose={handleClose} />
+      <div className="flex space-x-6">
+        {discountArr.map((discount) => (
+          <Card
+            key={discount.id}
+            item={discount}
+            showModal={showModal}
+            setShowModal={setShowModal}
+            handleUpdate={handleUpdate}
+          />
+        ))}
+      </div>
+      <Modal
+        visible={showModal}
+        onClose={handleClose}
+        setShowModal={setShowModal}
+        handleAdd={handleAdd}
+      />
     </div>
   );
 };
