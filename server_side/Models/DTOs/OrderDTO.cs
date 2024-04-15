6088eafc2e743268dc1a390807caad0f2b1fbd36
LeThinhPhuc <phuclethinh70@gmail.com>
2024-04-15 21:28:48 +0700

    db

---
+++ server_side/Models/DTOs/OrderDTO.cs
@@ -0,0 +1,21 @@
+﻿using CoffeeShopApi.Models.Abstract;
+using CoffeeShopApi.Models.DomainModels;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace CoffeeShopApi.DTOs
+{
+    public class OrderDTO : BaseEntity
+    {
+
+        [Required]
+        public DateTime? OrderDate { get; set; }
+
+        public double Total { get; set; }
+
+        public ApplicationUserDTO? User { get; set; }  // the staff who create(confirm) the order
+
+        public List<OrderItemDTO>? OrderItems { get; set; }
+
+    }
+}
---
+++ server_side/Repositories/Implements/OrderRepository.cs
@@ -0,0 +1,131 @@
+﻿using AutoMapper;
+using CoffeeShopApi.DTOs;
+using CoffeeShopApi.Models.DAL;
+using CoffeeShopApi.Models.DomainModels;
+using CoffeeShopApi.PostModels;
+using CoffeeShopApi.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace CoffeeShopApi.Repositories.Implements
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private AppDbContext context;
+        private readonly IMapper mapper;
+
+        public OrderRepository(AppDbContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+
+        public async Task<IEnumerable<OrderDTO>> GetAllOrdersAsync()
+        {
+            var s = context.Orders.Include(o => o.User)
+                .Include(o => o.OrderItems).ToList();
+
+            var orders = await context.Orders.Select(o => new OrderDTO
+            {
+                Id = o.Id,
+                OrderDate = o.OrderDate,
+                Total = o.Total,
+                User = new ApplicationUserDTO
+                {
+                    Id = o.User.Id,
+                    FullName = o.User.FullName
+                },
+                OrderItems = o.OrderItems.Select(orderItem => new OrderItemDTO
+                {
+                    OrderId = orderItem.Id,
+                    Quantity = orderItem.Quantity,
+                    Note = orderItem.Note,
+                    Drink = new DrinkDTO
+                    {
+                        DrinkId = orderItem.Drink.Id,
+                        Name = orderItem.Drink.Name,
+                        Price = orderItem.Drink.Price
+                    }
+                }).ToList()
+            }).ToListAsync();
+
+            return orders;
+        }
+
+
+        public async Task<OrderDTO> GetOrderByIdAsync(string id)
+        {
+            var orders = await context.Orders.Include(o => o.User)
+                .Include(o => o.OrderItems).FirstOrDefaultAsync(o => o.Id == id);
+
+            var orderToReturn = mapper.Map<OrderDTO>(orders);
+
+            return orderToReturn;
+        }
+
+        public async Task<OrderDTO> GetOrdersByUserIdAsync(string userId)
+        {
+            var orders = await context.Orders.Include(o => o.User)
+                .Include(o => o.OrderItems).FirstOrDefaultAsync(o => o.UserId == userId);
+
+            var orderToReturn = mapper.Map<OrderDTO>(orders);
+
+            return orderToReturn;
+        }
+
+
+        public async Task<bool> DeleteOrderAsync(string id)
+        {
+            var order = await context.Orders.FirstOrDefaultAsync(o => o.Id == id);
+
+            context.Remove(order);
+
+            await context.SaveChangesAsync();
+            if (order != null)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public async Task<OrderDTO> AddNewOrderAsync(OrderModelDTO newOrderDT0)
+        {
+            string newOrderId = Guid.NewGuid().ToString();//tự động tạo 1 mã order mới 
+            Order newOrder = new Order
+            {
+                Id = newOrderId,
+                UserId = newOrderDT0.UserId,
+                OrderDate = DateTime.Now,
+                Total = newOrderDT0.Total,
+                OrderItems = newOrderDT0.OrderItems.Select(o => new OrderItem
+                {
+                    OrderId = newOrderId,
+                    DrinkId = o.DrinkId,
+                    Quantity = o.Quantity,
+                    Note = o.Note
+                }).ToList(),
+            };
+
+            context.Orders.Add(newOrder);
+
+            int result = await context.SaveChangesAsync();
+
+            if (result > 0)
+            {
+                return mapper.Map<OrderDTO>(newOrder);
+            }
+
+            return null;
+        }
+
+
+        public async Task<OrderDTO> UpdateOrderAsync(OrderDTO orderChanges)
+        {
+            return new OrderDTO { Id = orderChanges.Id };
+        }
+
+
+
+    }
+}
