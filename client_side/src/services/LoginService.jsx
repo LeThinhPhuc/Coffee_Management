72632d149979ab5f2f12a2a2cf7be37e12bb0474
Counter Logic <tuandinhbi75@gmail.com>
2024-04-19 16:07:15 +0700

    jwt processing

--- client_side/src/services/LoginService.jsx
+++ client_side/src/services/LoginService.jsx
@@ -1,17 +1,18 @@
 import axios from 'axios'
-const Login = {
+const LoginService = {
 
-   Login: (body)=> axios.create({
+   doLogin: (body)=> axios.create({
         baseURL: "http://localhost:5146/",
         timeout: 5000,
         headers: {
             "Content-Type": "application/json",
             "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
             "Access-Control-Allow-Origin":"https://localhost:5173",
-            "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+            "Access-Control-Allow-Methods": "GET",
             Accept: "application/x-www-form-urlencoded, text/plain",
           },
     }).post('api/Auth/loginef',body)
 
 }
+
 export default LoginService;
