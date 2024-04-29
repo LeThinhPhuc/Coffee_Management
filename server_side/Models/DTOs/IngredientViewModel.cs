8b5b10da14351cdcda4af5d8f38d2c44643621f6
entykey <nguyentuan4026@gmail.com>
2024-04-29 14:21:40 +0700

    modified for Minh

--- server_side/Models/DTOs/IngredientViewModel.cs
+++ server_side/Models/DTOs/IngredientViewModel.cs
@@ -7,8 +7,11 @@ namespace CoffeeShopApi.Models.DTOs
         public string? Name { get; set; }
         public double? Amount { get; set; }
         public string? FormattedExpiryDate { get; set; }
-        public string? FormattedDateCreated { get; set; }  // for Minh
-        public string? FormattedDateModified { get; set; } // for Minh
+        public string? FormattedDateCreated { get; set; } 
+        public string? FormattedDateModified { get; set; }
+        public DateTime? ExpiryDate { get; set; }
+        public DateTime? DateCreated { get; set; }  // for Minh
+        public DateTime? DateModified { get; set; } // for Minh
         public string? ImagePath { get; set; }
     }
 }
